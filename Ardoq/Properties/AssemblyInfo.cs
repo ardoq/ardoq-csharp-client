66f63d74e02fda726102dc4d5da27e59fb93d2ec
Erik Bakstad <erik@ardoq.com>
2017-02-09 12:44:16 +0100

    Release 2.0.1

--- Ardoq/Properties/AssemblyInfo.cs
+++ Ardoq/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ardoq AS")]
 [assembly: AssemblyProduct("ardoq-csharp-client")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyVersion("2.0.1")]
 [assembly: AssemblyFileVersion("1.0.0")]
